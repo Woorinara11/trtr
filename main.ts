1cd99e9e33041285bc83670dadbc3963b13f74ea
Woorinara11 <107897918+Woorinara11@users.noreply.github.com>
2022-06-22 11:15:57 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,1 +1,48 @@
-
+let 가위바위보 = 0
+input.onButtonPressed(Button.A, function () {
+    basic.pause(1000)
+    basic.showNumber(game.score())
+})
+input.onGesture(Gesture.Shake, function () {
+    가위바위보 = randint(1, 3)
+    if (가위바위보 == 1) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    } else if (가위바위보 == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    game.setScore(0)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    game.addScore(1)
+    basic.pause(1000)
+    basic.showNumber(game.score())
+})
